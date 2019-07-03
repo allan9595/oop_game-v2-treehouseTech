358b4ce0965b8257ed815174adbb9b51eacec61f
*allan9595* <bohanzhang@my.unt.edu>
2019-07-03 15:09:52 -0400

    fix bug

--- js/app.js
+++ js/app.js
@@ -2,18 +2,18 @@
  * Project 4 - OOP Game App
  * app.js */
 
+let game = new Game();
 $("#btn__reset").on('click', () => {
-    const game = new Game();
+    
+    console.log(game);
     game.startGame();
-    
-    $(".keyrow button").on('click',(e) => {
-        game.handleInteraction(e.target);
-    })
-    
-    document.addEventListener('keyup',(e)=>{
-        game.handleInteractionKeyboard(e.key);
-    })
-    
+        
 })
 
+$(".keyrow button").on('click',(e) => {
+    game.handleInteraction(e.target);
+})
 
+document.addEventListener('keyup',(e)=>{
+    game.handleInteractionKeyboard(e.key);
+})
